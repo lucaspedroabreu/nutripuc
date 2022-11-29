9c47f97749218414e74b61c5c48ea6329da20fd1
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2022-11-29 16:58:02 -0300

    Correção do modal e página de signup

--- script.js
+++ script.js
@@ -1,62 +1,37 @@
 // exibe/oculta o menu em telas pequenas
-function showHideFunction () {
-    const menuStyleDisplay = document.querySelector("ul").style;
-    if (menuStyleDisplay.display == "flex") {
-        menuStyleDisplay.display = "none";
-    } else {
-        menuStyleDisplay.display = "flex";
-    }
-}
-
-// bloqueia o background e exibe um modal de login
-function showLoginModal () {
-    const bodyLockScreen = document.querySelector("body").style;
-    const modal = document.getElementsByClassName("login-bg")[0].style;
-    
-    bodyLockScreen.overflow = "hidden";
-    modal.display="flex";
-}
-
-// desbloqueia o background e oculta o modal de login
-function hideLoginModal () {
-    const bodyLockScreen = document.querySelector("body").style;
-    const modal = document.getElementsByClassName("login-bg")[0].style;
-    
-    bodyLockScreen.overflow = "";
-    modal.display="none";
+function showHideFunction() {
+  const menuStyleDisplay = document.querySelector('ul').style
+  if (menuStyleDisplay.display == 'flex') {
+    menuStyleDisplay.display = 'none'
+  } else {
+    menuStyleDisplay.display = 'flex'
+  }
 }
 
 // Identifica os Resizes e redefine os menus de naveção
-addEventListener("resize", () => {
-    const currentWidth = window.innerWidth;
-    if (currentWidth > 750) {
-        resetMenuDisplay(true);
-    }
-    else {
-        resetMenuDisplay(false);
-    }
+addEventListener('resize', () => {
+  const currentWidth = window.innerWidth
+  if (currentWidth > 750) {
+    resetMenuDisplay(true)
+  } else {
+    resetMenuDisplay(false)
+  }
 })
 
 // adiciona evento de click ao botão Menu
-const menuIcon = document.getElementsByClassName("menu-icon");
-menuIcon[0].addEventListener("click", showHideFunction);
+const menuIcon = document.getElementsByClassName('menu-icon')
+menuIcon[0].addEventListener('click', showHideFunction)
 
-function resetMenuDisplay (result) {
-    const menuStyleDisplay = document.querySelector("ul").style;
-    if (result) menuStyleDisplay.display = 'flex';
-    if (!result) menuStyleDisplay.display = 'none';
+function resetMenuDisplay(result) {
+  const menuStyleDisplay = document.querySelector('ul').style
+  if (result) menuStyleDisplay.display = 'flex'
+  if (!result) menuStyleDisplay.display = 'none'
 }
 
-// adiciona evento de click ao botão Login
-const loginBtn = document.getElementsByClassName("login");
-loginBtn[0].addEventListener("click", showLoginModal);
+// Bootstrap modal Javascript
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
 
-// adiciona evento de click ao botão Close-modal
-const loginClose = document.getElementsByClassName("close-modal");
-loginClose[0].addEventListener("click", hideLoginModal);
-
-function resetMenuDisplay (result) {
-    const menuStyleDisplay = document.querySelector("ul").style;
-    if (result) menuStyleDisplay.display = 'flex';
-    if (!result) menuStyleDisplay.display = 'none';
-}
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
